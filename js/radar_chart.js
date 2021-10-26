ed49dd05a8392447108b311100e55e157d3fc796
Syz03 <aakibdiscoo@gmail.com>
2021-10-26 22:36:51 +0545

    Updates with the Polar Area Chart

---
+++ js/radar_chart.js
@@ -0,0 +1,31 @@
+
+const CHART = document.getElementById("myChart");
+
+
+const data = {
+  labels: [
+    'HTML5',
+    'CSS3',
+    'JAVASCRIPT',
+    'BOOTSTRAP',
+    'REACT'
+  ],
+  datasets: [{
+    label: 'CODING SKILLS',
+    data: [10, 10, 7, 7, 5],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(75, 192, 192)',
+      'rgb(255, 205, 86)',
+      'rgb(201, 203, 207)',
+      'rgb(54, 162, 235)'
+    ]
+  }]
+};
+
+
+let radar = new Chart(CHART, {
+  type: 'polarArea',
+  data: data,
+  options: {}
+});
